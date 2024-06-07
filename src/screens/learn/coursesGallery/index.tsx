d683c5a27e92b9812d9c43694d57f5f08a1e3fbc
Giuseppe Ippoito <gippolito@hotmail.co.uk>
2024-06-07 20:02:01 +0700

    uncommented courseGallery main element

--- src/screens/learn/coursesGallery/index.tsx
+++ src/screens/learn/coursesGallery/index.tsx
@@ -71,28 +71,25 @@ const CoursesGallery = () => {
     }
 
     return (
-        <ErrorPage onPress={() => refreshData} />
-
-        // <ScrollView style={{
-        //     flex: 1,
-        //     backgroundColor: colors.primaryLight
-        // }}>
-        //     <View>
-        //         <FlatList
-        //             data={courses!}
-        //             renderItem={renderItem}
-        //             keyExtractor={(item, i) => `${item.id}-${i}`}
-        //         />
-        //     </View>
+        <ScrollView style={{
+            flex: 1,
+            backgroundColor: colors.primaryLight
+        }}>
+            <View>
+                <FlatList
+                    data={courses!}
+                    renderItem={renderItem}
+                    keyExtractor={(item, i) => `${item.id}-${i}`}
+                />
+            </View>
 
 
-        //     {/* <FlatList
-        //         data={posts!}
-        //         renderItem={renderItem}
-        //         keyExtractor={(item, i) => `${item.title}-${i}`}
-        //     /> */}
-        // </ScrollView>
-    )
+            {/* <FlatList
+                data={posts!}
+                renderItem={renderItem}
+                keyExtractor={(item, i) => `${item.title}-${i}`}
+            /> */}
+        </ScrollView>)
 }
 
 export default CoursesGallery
